5f259f59ef7796b2119bd23b855c4d40dbe6f60d
Amey Deshpande <ameydeshpande30@gmail.com>
2018-07-03 19:52:29 +0530

    login page added

--- data/views.py
+++ data/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 def loginPage(request):
     return render(request, "index.html")
+
+
+
+def questionadd(request):
+    tilte = request.POST["title"]
+    oa = request.POST["oa"]
+    ob = request.POST["ob"]
+    oc = request.POST["oc"]
+    od = request.POST["od"]
+    ans = request.POST["ans"]

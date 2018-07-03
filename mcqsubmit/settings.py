fc3f1a604c6436dd1df85424b6c3d5d79bdf3280
Amey Deshpande <ameydeshpande30@gmail.com>
2018-07-03 18:37:05 +0530

    login page added

--- mcqsubmit/settings.py
+++ mcqsubmit/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'hx%r**bsm%no^9-0aj213cuej5*e$0bko7_xstpun@@@lx@jhp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'data',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

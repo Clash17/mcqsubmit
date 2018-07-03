20171ec167793c4f4be825d6a7842c3a234fe570
omkar <omkarpatil0705@gmail.com>
2018-07-03 20:09:35 +0530

    signup

--- data/views.py
+++ data/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
+from django.http import  Httpresponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+
 
 
 # Create your views here.
+
+
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        ufname = request.POST.get('ufname')
+        ulname = request.POST.get('ulname')
+        uname = request.POST.get('uname')
+        uemail = request.POST.get('uemail')
+        upwd = request.POST.get('upwd')
+
+        user = User.objects.create_user(username = uname, email = uemail, password = upwd , first_name = ufname, last_name = ulname )
+
+        u2 = authenticate(request, username = uname , password = upwd)
+
+        login(request, u2)
+
+        return render(request, 'question.html')
+    else:
+        return render(request, 'index.html')
+
+
 def loginPage(request):
     return render(request, "index.html")

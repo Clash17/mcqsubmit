5f920ba4a8c87dd5a06b5981c77fea6015808214
Amey Deshpande <ameydeshpande30@gmail.com>
2018-07-03 19:29:32 +0530

    login page added

--- data/models.py
+++ data/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
+
 # Create your models here.
+class Questions(models.Model):
+    title = models.CharField(max_length=5000)
+    oa = models.CharField(max_length=1000)
+    ob = models.CharField(max_length=1000)
+    oc = models.CharField(max_length=1000)
+    od = models.CharField(max_length=1000)
+    ans = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.title
+
+
+class Queadd(models.Model):
+    qid = models.OneToOneField(Questions, default=0, on_delete=models.CASCADE)
+    uid = models.ForeignKey(User, default=0, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
